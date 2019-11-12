41b58c99312cc92127d738d3068877a8b96afd54
Jess Daniel <jessdaniel474@gmail.com>
2019-11-12 17:13:29 -0500

    Added axiosWithAuth get call to get friends

--- friends/src/components/FriendsList.jsx
+++ friends/src/components/FriendsList.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import Friend from "./Friend";
 
 const FriendsList = () => {
+  const [friends, setFriends] = useState([]);
+
+  const axiosWithAuth = () => {
+    return axios.create({
+      headers: {
+        authorization: sessionStorage.getItem("token")
+      }
+    });
+  };
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get("http://localhost:5000/api/friends")
+      .then(res => {
+        console.log(res.data);
+        setFriends(res.data);
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
+  }, []);
   return (
     <>
       <h3>Friends List</h3>
+      {friends.map(friend => (
+        <Friend key={friend.id} friend={friend} />
+      ))}
     </>
   );
 };
